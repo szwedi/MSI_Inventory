d75606cb51815c77ea778e217f67b5528281ad90
szwedi <szwedowski.adam@gmail.com>
2014-04-06 15:18:00 +0200

    Get object to inventory

--- Assets/Scripts/InventoryControl.cs
+++ Assets/Scripts/InventoryControl.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class InventoryControl : MonoBehaviour {
 
@@ -8,24 +8,50 @@ public class InventoryControl : MonoBehaviour {
 	public float inventoryHeight = 325;
 
 	private bool showInventory = false;
+	private bool showLabelGetItem = false;
 
 	public RaycastHit hit;
 	private Ray ray;
 
+	public Slot[,] slots;
+	private int slotsX = 5;
+	private int slotsY = 3;
+
 	// Use this for initialization
 	void Start () {
 
+		//initialization slots
+		slots = new Slot[slotsX, slotsY];
+		for (int tmpX = 0; tmpX < slotsX; tmpX++) {
+			for (int tmpY = 0; tmpY < slotsY; tmpY++) {
+				slots[tmpX, tmpY] = new Slot();
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		//display label to get item
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		if (Physics.Raycast (ray, out hit,3)){
-			if (hit.transform.tag == "item")
-				Debug.Log ("Hit " + hit.transform.name);
+		if (Physics.Raycast (ray, out hit, 2) && hit.transform.tag == "item") {
+			showLabelGetItem = true;
+			//get item to slots
+			if (Input.GetKeyDown (KeyCode.E)) {
+				addItemToSlots(hit.transform.gameObject);
+				Destroy (hit.transform.gameObject);
+				if (slots[1,0].item != null)	
+					Debug.Log("0 " + slots[1,0].item.name);
+				if (slots[1,1].item != null)
+					Debug.Log("1 " + slots[1,1].item.name);
+				if (slots[1,2].item != null)
+					Debug.Log("2 " + slots[1,2].item.name);
+			}
+		} else {
+			showLabelGetItem = false;
 		}
 
+		//open inventory
 		if (Input.GetKeyDown (KeyCode.I)) {
 			if (showInventory == false) {
 				showInventory = true;
@@ -33,10 +59,30 @@ public class InventoryControl : MonoBehaviour {
 				showInventory = false;
 			}
 		}
-	}
 
+	}
+	
+	//Display GUI
 	void OnGUI() {
+
+		if (showLabelGetItem == true)
+			GUI.Label (new Rect (Screen.width / 2-100, Screen.height*0.8f, 200, 50), "Press E to get item");
+
 		if (showInventory == true)
 			GUI.DrawTexture(new Rect(Screen.width/2-inventoryWidth/2,Screen.height/2-inventoryHeight/2,inventoryWidth,inventoryHeight), inventoryBackground);	
+	}
+
+	//Add item to first not occupieted slot main inventory  
+	void addItemToSlots(GameObject item)
+	{
+		for (int tmpX = 1; tmpX < slotsX; tmpX++) {
+			for (int tmpY = 0; tmpY < slotsY; tmpY++) {
+				if (slots[tmpX, tmpY].occupied == false){
+					slots [tmpX, tmpY].item = item;
+					slots [tmpX, tmpY].occupied = true;
+					return;
+				}
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Slot {
+
+	public bool occupied = false;
+	public GameObject item = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
